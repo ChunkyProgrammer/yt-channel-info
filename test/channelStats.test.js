e80561f0be9530a95ca4830c1fda433197e5bd2c
onionchunky <78101139+ChunkyProgrammer@users.noreply.github.com>
2022-04-09 15:38:15 -0400

    fix tests
    comment out tests that are uncertain at the moment

--- test/channelStats.test.js
+++ test/channelStats.test.js
@@ -1,10 +1,17 @@
+const { test } = require('@jest/globals')
 const ytch = require('../index')
 /* eslint no-undef: "off" */
 describe('Channel stats', () => {
   test('Channel stats', () => {
     const parameters = { channelId: 'UCMO51vS4kaOSLqBD9bmZGIg', channelIdType: 1 }
     return ytch.getChannelStats(parameters).then((data) => {
-      expect(data.joinedDate).toBe(0)
+      expect(data.joinedDate).toBe(1355202000000)
+    })
+  })
+  test('Channel with videos', () => {
+    const parameters = { channelId: 'UCfMJ2MchTSW2kWaT0kK94Yw', channelIdType: 1 }
+    return ytch.getChannelStats(parameters).then((data) => {
+      expect(data.viewCount).not.toBe(0)
     })
   })
 })
